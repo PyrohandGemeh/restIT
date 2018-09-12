7096e75b0679595ab919fe517e79c73a4ccbc438
Alessandra Rota <35770400+alerota@users.noreply.github.com>
2018-09-12 18:47:37 +0200

    Sistemato due cose

--- app/core/App.php
+++ app/core/App.php
@@ -7,48 +7,32 @@
  */
 
 class App {
-
     protected $controller = 'index';
     protected $method = 'indexAction';
     protected $params = [];
-
     public function __construct()
     {
-        if(isset($_GET['url']))
-            $url = $this->parseUrl();
-        else
-            $url = array('/pollo');
-
+        $url = $this->parseUrl();
         if(file_exists('app/controllers/'. $url[0] .'.php')) {
             $this->controller = $url[0];
             unset($url[0]);
         }
-
         require_once 'app/controllers/'. $this->controller .'.php';
-
         $this->controller = new $this->controller;
-
         if(isset($url[1])) {
             $methodName = $url[1];
-
             if ($_SERVER['REQUEST_METHOD'] == 'POST')
                 $methodName .= 'Post';
-
             //echo $methodName;
-
             if(method_exists($this->controller, $methodName. 'Action')) {
                 $this->method = $methodName. 'Action';
                 //echo $this->method;
                 unset($url[1]);
             }
         }
-
         $this->params = $url ? array_values($url) : [];
-
         call_user_func_array([$this->controller, $this->method], $this->params);
-
     }
-
     public function parseUrl() {
             return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
 
--- app/views/index.php
+++ app/views/index.php
@@ -10,7 +10,7 @@ echo $data['result'];
 
 ?>
 
-<form action="../utenti/login" method="post" name="login">
+<form action="utenti/login" method="post" name="login">
     <input type="text" name="username" placeholder="username">
     <input type="password" name="password" placeholder="password">
     <input type="submit" name="login" value="Login">
